0df3945905263ba3b92a618d8bc3b3fc9f77f352
Jamtech Dev <100343101+mohit-nigam77@users.noreply.github.com>
2024-07-02 18:37:03 +0530

    working_on_product_vsPagePage_mobile_table_neet

--- src/components/Common/MobileCompareTable/MobileCompareTable.jsx
+++ src/components/Common/MobileCompareTable/MobileCompareTable.jsx
@@ -1395,6 +1395,27 @@ export default function MobileCompareTable({
                                                         {data?.when_matters}
                                                       </p>
                                                     )}
+                                                    {data?.importance && (
+                                                      <p className="mb-2">
+                                                        <b>
+                                                          {productPhaseData &&
+                                                            productPhaseData?.importance_text}
+                                                          :{" "}
+                                                        </b>{" "}
+                                                        {data?.importance}
+                                                      </p>
+                                                    )}
+
+                                                    {data?.good_value && (
+                                                      <p className="mb-2">
+                                                        <b>
+                                                          {productPhaseData &&
+                                                            productPhaseData?.good_value_text}
+                                                          :{" "}
+                                                        </b>{" "}
+                                                        {data?.good_value}
+                                                      </p>
+                                                    )}
                                                   </div>
                                                 )}
                                                 <svg
